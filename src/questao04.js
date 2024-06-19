7f6c0ce1bc6730e22feb5f27e3a35c2f0be8b3f5
ValerioMeurerJR <valerio.meurer_jt@live.com>
2024-06-18 21:28:17 -0300

    Refazendo atividade 4

--- src/questao04.js
+++ src/questao04.js
@@ -2,28 +2,28 @@ import leia from "readline-sync"
 
 var Nota = leia.questionFloat("INFORME NOTA: ");
 
-if (Nota < 0 || Nota > 10) {
+if (Nota < 0.0 || Nota > 10.0) {
     console.log("NOTA INVALIDA");
 } else {
     switch (Nota) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
+        case 1.0:
+        case 2.0:
+        case 3.0:
+        case 4.0:
+        case 5.0:
             console.log("NOTA F")
             break;
-        case 6:
+        case 6.0:
             console.log("NOTA D")
             break;
-        case 7:
+        case 7.0:
             console.log("NOTA C")
             break;
-        case 8:
+        case 8.0:
             console.log("NOTA B")
             break;
-        case 9:
-        case 10:
+        case 9.0:
+        case 10.0:
             console.log("NOTA A")
             break;
         default:
